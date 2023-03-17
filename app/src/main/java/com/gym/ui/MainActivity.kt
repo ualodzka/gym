a54ddb17186e74bd81350c558e30fa24f7791530
levl <v.lezhnevich@gmail.com>
2023-03-17 23:04:08 +0100

    Upgrade kotlin version and some other dependencies

--- app/src/main/java/com/gym/ui/MainActivity.kt
+++ app/src/main/java/com/gym/ui/MainActivity.kt
@@ -1,16 +1,36 @@
 package com.gym.ui
 
+
 import android.os.Bundle
+
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.gym.ui.theme.GymTheme
+// for a 'val' variable
+import androidx.compose.runtime.getValue
+
+// for a `var` variable also add
+import androidx.compose.runtime.setValue
+
+// or just
+import androidx.compose.runtime.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,8 +38,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             GymTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    EditSetCard("World!")
                 }
             }
         }
@@ -27,14 +50,47 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun EditSetCard(name: String) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(12.dp, 12.dp, 12.dp, 12.dp))
+            .background(MaterialTheme.colorScheme.primaryContainer)
+            .padding(start = 16.dp, end = 16.dp, top = 8.dp, bottom = 8.dp)
+    ) {
+        Row() {
+            Text(
+                fontSize = 16.sp,
+                text = "Lat Pull down",
+                color = MaterialTheme.colorScheme.onPrimaryContainer,
+                modifier = Modifier
+                    .wrapContentWidth()
+
+            )
+        }
+        Row {
+            Text(text = "1")
+            Input()
+            Text(text = "Hello $name!")
+        }
+    }
+}
+
+@Composable
+fun Input() {
+    var text by remember { mutableStateOf("") }
+
+    OutlinedTextField(
+        value = text,
+        onValueChange = { text = it },
+        label = { Text("Label") }
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     GymTheme {
-        Greeting("Android")
+        EditSetCard("World")
     }
 }
---
+++ data/src/main/java/com/gym/data/Repo.kt
@@ -0,0 +1,11 @@
+package com.gym.data
+
+import android.content.Context
+
+
+interface Repo {
+
+    fun saveSet(context: Context)
+
+}
+
